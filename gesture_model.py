72a86c36defea20ffcaa1e9b1bc7676ca81221ba
Aman Sharma <amansharma2910@gmail.com>
2021-01-08 16:01:45 +0530

    Started the project

---
+++ gesture_model.py
@@ -0,0 +1,11 @@
+# importing project dependencies
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cv2 as cv
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+
