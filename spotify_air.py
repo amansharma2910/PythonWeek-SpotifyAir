d0ae0bc401ffa26dc8b4a220956e0811439153b0
Aman Sharma <amansharma2910@gmail.com>
2021-01-09 22:14:40 +0530

    Started working on CV part

---
+++ spotify_air.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import numpy
+import cv2 as cv
+
+# model class
+class GestureModel(nn.Module):
+    """Neural net for recognizing hand gestures
+    Input dims: m x 1 x 28 x 28
+    Output dims: m x 4
+    """
+    def __init__(self):
+        super().__init__()
+        # input: m x 1 x 28 x 28
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+        ) # m x 16 x 28 x 28
+        self.conv2 = nn.Sequential(
+            # nn.Dropout(p=0.2),
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2),
+        ) # m x 32 x 14 x 14
+        self.conv3 = nn.Sequential(
+            # nn.Dropout(p=0.2),
+            nn.Conv2d(32, 64, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2),
+        ) # m x 64 x 6 x 6
+        self.conv4 = nn.Sequential(
+            # nn.Dropout(p=0.2),
+            nn.Conv2d(64, 128, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2),
+        ) # m x 128 x 2 x 2
+        self.classifier = nn.Sequential(
+            nn.Flatten(), # m x 128*2*2
+            nn.Dropout(p=0.2),
+            nn.Linear(128*2*2, 4),
+            nn.Softmax(),
+        ) # m x 4
+
+    def forward(self, input):
+        x = self.conv1(input)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        output = self.classifier(x)
+        return output
+
+model = torch.load("hand_gesture_model.pth")
+print(model)
+# def frameResize(frame, scale = 0.75):
+#     height = int(frame.shape[0] * scale)
+#     width = int(frame.shape[1] * scale)
+#     dimensions = (width, height)
+
+#     resizedFrame = cv.resize(frame, dimensions, interpolation= cv.INTER_AREA)
+#     return resizedFrame
+
+# video = cv.VideoCapture(0)
+# while True:
+#     isTrue, frame = video.read()
+#     print(type(frame))
+#     break
